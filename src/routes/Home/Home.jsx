84b291456688cd4531c8c463e60b1f355845cd29
Vinicius <viniciusbittencourt113@gmail.com>
2024-10-10 09:56:54 -0300

    Ajustes

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,7 +1,29 @@
+import { useState, useEffect } from "react"
+
 const Home = () => {
+    
+    const [listaProduto, setListaProduto] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/produto")
+        .then((res) =>{
+            return res.json();
+        })
+        .then((data) => {
+            setListaProduto(data)
+        })
+    }, [])
+
     return(
         <>
-            <h1>Home</h1>
+            <div>
+                {listaProduto.map((item, index) => (
+                    <div key={index}>
+                        <h1>{item.nome}</h1>
+                        <img src={item.foto}/>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
